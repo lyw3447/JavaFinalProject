33212efb6163dcd5420f619316cb12ad76532640
lyw3447 <lyw3447@naver.com>
2019-06-07 00:15:42 +0900

    fill it out as original

--- src/main/java/edu/handong/analysis/utils/Utils.java
+++ src/main/java/edu/handong/analysis/utils/Utils.java
@@ -1,5 +1,66 @@
 package edu.handong.analysis.utils;
 
-public class Utils {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
+public class Utils {
+	public static void writeFile(ArrayList<String> lines, String fileName)
+	{
+		Path path = Paths.get(fileName);
+		File parent = path.toFile().getParentFile();
+		if (!parent.exists()) {
+			try {
+				parent.mkdirs();
+			}
+			catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
+		File resultFile = new File(fileName);
+		
+		try {
+			PrintWriter pw = new PrintWriter(new FileOutputStream(resultFile));
+			resultFile.createNewFile();
+			pw.println("StudentID, TotalNumberOfSemestersRegistered, Semester, NumCoursesTakenInTheSemester");
+			for (String line : lines)
+				pw.println(line);
+			pw.close();
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} 
+	}	
+	
+	public static void writeFile2(ArrayList<String> lines, String fileName) {
+		Path path = Paths.get(fileName);
+		File parentDir = path.toFile().getParentFile();
+		if (!parentDir.exists()) {
+			try {
+				parentDir.mkdirs();
+			}
+			catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		
+		File resultFile = new File(fileName);
+		
+		try {
+			PrintWriter pw = new PrintWriter(new FileOutputStream(resultFile));
+			resultFile.createNewFile();
+			pw.println("Year,Semester,CouseCode, CourseName,TotalStudents,StudentsTaken,Rate");
+			for (String str : lines)
+				pw.println(str);
+			pw.close();
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
